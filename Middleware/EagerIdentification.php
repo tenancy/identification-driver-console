58818c7fe3c4f0405466c43b877410ff8a8aa52e
Daniël Klabbers <daniel@klabbers.email>
2018-07-11 02:08:15 +0200

    added console identification

---
+++ Middleware/EagerIdentification.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Identification\Drivers\Console\Middleware;
+
+use Closure;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Contracts\Foundation\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Tenancy\Environment;
+
+class EagerIdentification
+{
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function handle(CommandStarting $event)
+    {
+        /** @var Environment $tenancy */
+        $tenancy = $this->app->make(Environment::class);
+
+        if (! $tenancy->isIdentified()) {
+            $this->app->instance(InputInterface::class, $event->input);
+            $tenancy->getTenant();
+        }
+    }
+}
